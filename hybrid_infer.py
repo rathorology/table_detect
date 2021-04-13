aea9fe4d8cc69d2b847f608f04d10d025419ea89
Abhishek Singh Kushwah <abhishek.kushwah@vengage.ai>
2021-04-13 14:21:18 +0200

    Add hybrid inference

--- hybrid_infer.py
+++ hybrid_infer.py
@@ -85,29 +85,21 @@ def run_inference_for_single_image(model, image):
 
 ########################################################################################################################
 
-
-def display(img):
-    # plt.rcParams["figure.figsize"] = (1280, 720)
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.show()
-
-
 def _square(x):
     return x * x
 
-
 def cie94(L1_a1_b1, L2_a2_b2):
-    # """Calculate color difference by using CIE94 formulae
-    #
-    # See http://en.wikipedia.org/wiki/Color_difference or
-    # http://www.brucelindbloom.com/index.html?Eqn_DeltaE_CIE94.html.
-    #
-    # cie94(rgb2lab((255, 255, 255)), rgb2lab((0, 0, 0)))
-    # >>> 58.0
-    # cie94(rgb2lab(rgb(0xff0000)), rgb2lab(rgb('#ff0000')))
-    # >>> 0.0
-    # """
-
+    """Calculate color difference by using CIE94 formulae
+    
+    See http://en.wikipedia.org/wiki/Color_difference or
+    http://www.brucelindbloom.com/index.html?Eqn_DeltaE_CIE94.html.
+    
+    cie94(rgb2lab((255, 255, 255)), rgb2lab((0, 0, 0)))
+    >>> 58.0
+    cie94(rgb2lab(rgb(0xff0000)), rgb2lab(rgb('#ff0000')))
+    >>> 0.0
+    """
+    
     L1, a1, b1 = L1_a1_b1
     L2, a2, b2 = L2_a2_b2
 
@@ -119,18 +111,70 @@ def cie94(L1_a1_b1, L2_a2_b2):
     delta_b = b1 - b2
     delta_H_square = _square(delta_a) + _square(delta_b) - _square(delta_C)
     return (sqrt(_square(delta_L)
-                 + _square(delta_C) / _square(1.0 + 0.045 * C1)
-                 + delta_H_square / _square(1.0 + 0.015 * C1)))
+            + _square(delta_C) / _square(1.0 + 0.045 * C1)
+            + delta_H_square / _square(1.0 + 0.015 * C1)))
 
+def get_traingle(pts, img):
+
+    ## (1) Crop the bounding rect
+    rect = cv2.boundingRect(pts)
+    x, y, w, h = rect
+    croped = img[y:y + h, x:x + w].copy()
+
+    ## (2) make mask
+    pts = pts - pts.min(axis=0)
+
+    mask = np.zeros(croped.shape[:2], np.uint8)
+    cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
+
+    ## (3) do bit-op
+    dst = cv2.bitwise_and(croped, croped, mask=mask)
+
+    ## (4) add the white background
+    bg = np.ones_like(croped, np.uint8) * 255
+    cv2.bitwise_not(bg, bg, mask=mask)
+    dst2 = bg + dst
+    #display(dst2)
+    return dst2
+
+def compute_left_right_trgs_from_table(box_corners, img):
+    # Left
+    count = 0
+    out_triangles_left = []
+    prev = box_corners[0][0]
+    for i, j in zip(range(box_corners[0][0], box_corners[0][0] + int((box_corners[2][0]-box_corners[0][0])/3), 20), [box_corners[1][1]]*20):
+
+            pts = np.array([box_corners[0], (i, j), [prev, j]], np.int32).reshape((-1, 1, 2))
+            dst2 = get_traingle(pts, img)
+            out_triangles_left.append({"src":dst2, "c2d":(prev,j)})
+            prev = i
+            
+    out_triangles_left.pop(0)
+    
+    # Right
+    count = 0
+    out_triangles_right = []
+    prev = box_corners[3][0]
+
+    for i, j in zip(range(box_corners[3][0] - int((box_corners[2][0] - box_corners[0][0]) / 3), box_corners[3][0], 20),
+                    [box_corners[2][1]] * 20):
+        pts = np.array([box_corners[3], (i, j), [prev, j]], np.int32).reshape((-1, 1, 2))
+        dst2 = get_traingle(pts, img)
+
+        out_triangles_right.append({"src":dst2, "c2d":(prev,j)})
+        prev = i
+    out_triangles_right.pop(0)
+    out_triangles_right = out_triangles_right[::-1]
+    
+    return out_triangles_left, out_triangles_right
 
 def extract_unq_colors(img):
-    colors, pixel_count = extcolors.extract_from_image(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
-
+    colors, pixel_count =  extcolors.extract_from_image(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
+    
     return colors
 
-
 def compare_two_triangles(x, y):
-    # res = list(set(extract_unq_colors(trg1))^set(extract_unq_colors(trg2)))
+    #res = list(set(extract_unq_colors(trg1))^set(extract_unq_colors(trg2)))
     x = extract_unq_colors(x)
     y = extract_unq_colors(y)
     x = list(zip(*x))[0]
@@ -140,27 +184,42 @@ def compare_two_triangles(x, y):
     sum_ = []
     for i, j in zip(x1, y1):
         print(i, j)
-        sum_.append(int(cie94(cv2.cvtColor(np.uint8([[i]]), cv2.COLOR_BGR2Lab)[0][0],
-                              cv2.cvtColor(np.uint8([[j]]), cv2.COLOR_BGR2Lab)[0][0])))
+        sum_.append(int(cie94(cv2.cvtColor(np.uint8([[i]]), cv2.COLOR_BGR2Lab)[0][0], cv2.cvtColor(np.uint8([[j]]), cv2.COLOR_BGR2Lab)[0][0])))
     return x1, y1, sum(sum_)
 
-
-def all_traingles(out_traingles):
+def all_traingles(out_traingles=None):
     trgs = []
 
     for i in range(0, len(out_traingles) - 1):
         try:
-            print("********************************" + str(i) + "--" + str(i + 1) + "********************************")
-            l1, l2, s = compare_two_triangles(out_triangles[i], out_triangles[i + 1])
-            trgs.append((l1, l2, s, (i, i + 1)))
+            l1, l2, s = compare_two_triangles(out_traingles[i]["src"], out_traingles[i + 1]["src"])
+            trgs.append((l1, l2, s, (i, i + 1), (out_traingles[i]["c2d"], out_traingles[i + 1]["c2d"])))
             # display(out_triangles[i])
             # display(out_triangles[i + 1])
             print("********************************" + str(i) + "--" + str(i + 1) + "********************************")
         except:
             pass
-    return trgs
+    s_max = sorted(zip(list(zip(*trgs))[2], list(zip(*trgs))[4]), reverse=True)[:3]
+    return trgs, s_max
 
+def get_actual_bb(box_corners, img):
+    out_triangles_left, out_triangles_right = compute_left_right_trgs_from_table(box_corners, img)
+    left_trgs, s_max_left = all_traingles(out_triangles_left)
+    right_trgs, s_max_right = all_traingles(out_triangles_right)
 
+    # Draw Rect
+    bl = box_corners[0]
+    br = box_corners[3]
+    tl = s_max_left[2][1][0] # Can change this if needed
+    tr = s_max_right[2][1][0] # Can change this if needed
+    
+    # To draw
+    #pts = np.array([bl, tl, tr, br], np.int32)
+    #pts = pts.reshape((-1,1,2))
+    #display(cv2.polylines(img,[pts],True,(0,0,0)))
+    
+    return [bl, tl, tr, br]
+    
 for image_path in TEST_IMAGE_PATHS:
     st = time.time()
     # the array based representation of the image will be used later in order to prepare the
@@ -173,37 +232,8 @@ for image_path in TEST_IMAGE_PATHS:
     rect_box = output_dict['detection_boxes'][0] * np.array(
         [img.shape[0], img.shape[1], img.shape[0], img.shape[1]])
     ymin, xmin, ymax, xmax = rect_box[0], rect_box[1], rect_box[2], rect_box[3]
-    box_corners = [(int(xmin), int(ymin)), (int(xmax), int(ymin)), (int(xmax), int(ymax)), (int(xmin), int(ymax))]
+    box_corners = [(int(xmin), int(ymax)), (int(xmin), int(ymin)), (int(xmax), int(ymin)), (int(xmax), int(ymax))] # bl, tl, tr, br
 
-    count = 0
-    out_triangles = []
-    prev = box_corners[0][0]
-    for i, j in zip(range(box_corners[0][0], box_corners[0][0] + int((box_corners[2][0] - box_corners[0][0]) / 3), 20),
-                    [box_corners[1][1]] * 20):
-        pts = np.array([box_corners[0], (i, j), [prev, j]], np.int32).reshape((-1, 1, 2))
+    act_box_corners = get_actual_bb(box_corners, img)
 
-        ## (1) Crop the bounding rect
-        rect = cv2.boundingRect(pts)
-        x, y, w, h = rect
-        croped = img[y:y + h, x:x + w].copy()
-
-        ## (2) make mask
-        pts = pts - pts.min(axis=0)
-
-        mask = np.zeros(croped.shape[:2], np.uint8)
-        cv2.drawContours(mask, [pts], -1, (255, 255, 255), -1, cv2.LINE_AA)
-
-        ## (3) do bit-op
-        dst = cv2.bitwise_and(croped, croped, mask=mask)
-
-        ## (4) add the white background
-        bg = np.ones_like(croped, np.uint8) * 255
-        cv2.bitwise_not(bg, bg, mask=mask)
-        dst2 = bg + dst
-        out_triangles.append(dst2)
-        prev = i
-    out_triangles.pop(0)
-
-    # l1, l2, s = compare_two_triangles(out_triangles[10], out_triangles[11])
-    a = all_traingles(out_triangles)
-    print(a)
+    print(act_box_corners)
